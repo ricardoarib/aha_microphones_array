65d1b163dc637b51837836eb327a5a7f2ff59474
Ricardo Ribeiro <ribeiro@isr.ist.utl.pt>
2018-04-14 04:37:02 +0100

    Prepare tunning data for seb/jfmp with geo/srp algorithms.

--- matlab_scripts/calc_points.m
+++ matlab_scripts/calc_points.m
@@ -15,20 +15,24 @@ true_ang_cell = cell(length(th), length(N));
 
 for n = 1:length(N)
     n
-    myFile = selectData('seb', N(n),[], [], [], 'geo', 0, [],[],'T');
+%    myFile = selectData('seb', N(n),[], [], [], 'geo', 0, [],[],'T');
+%    myFile = selectData('seb', N(n),[], [], [], 'geo', 0, [],[],'V');
 %    myFile = selectData('seb', N(n),[], [], [], 'srp', 0, [],[],'T');
+%    myFile = selectData('seb', N(n),[], [], [], 'srp', 0, [],[],'V');
 %    myFile = selectData('jfmp', N(n),[], [], [], 'geo', 0, [],[],'T');
+%    myFile = selectData('jfmp', N(n),[], [], [], 'geo', 0, [],[],'V');
 %    myFile = selectData('jfmp', N(n),[], [], [], 'srp', 0, [],[],'T');
+    myFile = selectData('jfmp', N(n),[], [], [], 'srp', 0, [],[],'V');
 %     myFile = selectData([], [],[], [3], [], 'geo', 1, [],[],[]); %% test signals
     for o = 1:length(myFile)
         o
         [angles, ~, levels_dB] = load_data(fullfile( myFile(o).folder, myFile(o).name));
         tmpStruct = createFileStructure( myFile(o) );
-        true_ang = position_vec( tmpStruct.position )
+        true_ang = position_vec( tmpStruct.position );
         for m = 1:length(th)
-            m
+            %m
             [filtered_angles, ~] = filter_data (angles, levels_dB, th(m));
-            filtered_angles
+            %filtered_angles
             if ~isempty(filtered_angles)
                 ang = mean( filtered_angles ) ;
             else
