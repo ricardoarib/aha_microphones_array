832145b32dc0b26f92657c788e8dc0219e022bd8
Ricardo Ribeiro <ribeiro@isr.ist.utl.pt>
2018-04-15 17:26:40 +0100

    Correct count of phrases. Print values for errors and detection ratio.

--- matlab_scripts/plot_points.m
+++ matlab_scripts/plot_points.m
@@ -8,8 +8,8 @@
 %load('points_jfmp_srp_tunning.mat');
 %load('points_jfmp_srp_validation.mat');
 
-load('points_seb_geo_TV.mat');
-%load('points_seb_srp_TV.mat');
+%load('points_seb_geo_TV.mat');
+load('points_seb_srp_TV.mat');
 %load('points_jfmp_geo_TV.mat');
 %load('points_jfmp_srp_TV.mat');
 
@@ -40,8 +40,10 @@ load('points_seb_geo_TV.mat');
 
 % m = 1;
 m = find( th == -20 );
+%m = find( th == -15 );
 %n = 1;
 n = find( N == 16384 ) ;
+%n = find( N == 32768 ) ;
 
 %err = zeros(13, 420);
 %err = cell2mat(ang_cell{m,n}(:)) - cell2mat(true_ang_cell{m,n}(:));
@@ -59,7 +61,8 @@ row = @(x)reshape(x,1,numel(x)); % function to reshape any matrix to a row vecto
 cell2col    = @(C)col( cell2mat(row(C)) ); % function to convert cells to a single column vector
 cell2row    = @(C)row( cell2mat(row(C)) ); % function to convert cells to a single row vector
 
-cell2counts = @(C)row( cellfun(@(x)sum(~isnan(x)),C) ); % function to count valid points in cells. Returns a row vector.
+cell2counts      = @(C)row( cellfun(@(x)sum(~isnan(x)),C) ); % function to count valid points in cells. Returns a row vector.
+cell2countstotal = @(C)row( cellfun(@(x)length(x),C) ); % function to count all points in cells (valid+invalid). Returns a row vector.
 
 % functions to get a column[/row] vector with the id of the cells (id's are arbitrary increasing numbers)
 cellid2col = @(C)col( cell2mat( arrayfun(@(I)I*ones(numel(C{I}),1),col(1:numel(C)),'uni',0) ) );
@@ -73,14 +76,15 @@ stdrow     = @(M,Mid) row( arrayfun( @(x)   std( selectbyid(M,Mid,x), 'omitnan' 
 
 
 plot2=@(C,varargin)plot( cellid2col(C), cell2col(C), varargin{:} );
-plot3=@(C,X,varargin)plot( X(cellid2col(C)), cell2col(C), varargin{:} );
+plot4=@(C,X,varargin)plot( X(cellid2col(C)), cell2col(C), varargin{:} );
 boxplot2=@(C,varargin)boxplot( cell2row(C), cellid2row(C) , varargin{:} );
-boxplot3=@(C,X,varargin)boxplot( cell2row(C), X(cellid2row(C)) , varargin{:} );
+boxplot4=@(C,X,varargin)boxplot( cell2row(C), X(cellid2row(C)) , varargin{:} );
 
 
-angles      = cell2col(   ang_cell(m,:) );
-true_angles = cell2col(   true_ang_cell(m,:) );
-counts      = cell2counts(   ang_cell(m,:) );
+angles       = cell2col(   ang_cell(m,:) );
+true_angles  = cell2col(   true_ang_cell(m,:) );
+counts       = cell2counts(   ang_cell(m,:) );
+total_counts = cell2countstotal ( ang_cell(m,:) );
 err = angles - true_angles ;
 err = err + 360 * (err <= -180) ;
 err = err - 360 * (err > 180) ;
@@ -91,11 +95,18 @@ err_means   =   meanrow(err,ids) ;
 err_var     =    varrow(err,ids) ;
 err_std     =    stdrow(err,ids) ;
 
+disp( sprintf('err_medians [ N=%d th=%d ] = %.1f', N(n), th(m), err_medians(n)) )
+disp( sprintf('err_std     [ N=%d th=%d ] = %.1f', N(n), th(m), err_std(n)) )
+disp( sprintf('detected    [ N=%d th=%d ] = %.1f %%', N(n), th(m), counts(n)/total_counts(n)*100) )
+
+
+figure(1)
+
 subplot(4,2,1)
 %plot(x,angles,'o')
 plot(x,err,'o')
 %plot2( ang_cell(m,:), 'o' )
-%plot3( ang_cell(m,:), N, 'o' )
+%plot4( ang_cell(m,:), N, 'o' )
 set(gca,'xscale','log')
 set(gca,'xtick',N, 'XMinorTick','off')
 grid on
@@ -108,14 +119,14 @@ subplot(4,2,2)
 %boxplot( err, x, 'notch', 'on' )
 boxplot( err, x, 'notch', 'on', 'Widths', .8*counts/max(counts)+.0001 )
 %boxplot2( ang_cell(m,:), 'notch', 'on' )
-%boxplot3( ang_cell(m,:), N, 'notch', 'on' )
+%boxplot4( ang_cell(m,:), N, 'notch', 'on' )
 grid on
 xlabel('window size (N) [samples]')
 ylabel('angle error (estimated-true) [degrees]')
 title(sprintf('threshold = %d dBFS \n Box widths are proportional to sample size.',th(m)))
 set(gca,'ytick',[-180 -90 0 90 180])
 
-subplot(4,2,3)
+subplot(4,2,[1 3])
 plot( N', [err_medians' err_means' err_var'/1000 err_std'], '*-' )
 grid on
 set(gca,'xscale','log')
@@ -129,7 +140,8 @@ legend('median','mean','variance/1000','std')
 set(gca,'ytick',[-180:45:180])
 
 subplot(4,2,4)
-plot( N, counts/max(counts)*100, '*-' )
+%plot( N, counts/max(counts)*100, '*-' )
+plot( N, counts./total_counts*100, '*-' )
 grid on
 set(gca,'xscale','log')
 set(gca,'xtick',N, 'XMinorTick','off','XMinorGrid','off')
@@ -142,6 +154,7 @@ title(sprintf('threshold = %d dBFS',th(m)))
 angles      = cell2col(   ang_cell(:,n) );
 true_angles = cell2col(   true_ang_cell(:,n) );
 counts      = cell2counts(   ang_cell(:,n) );
+total_counts = cell2countstotal ( ang_cell(:,n) );
 err = angles - true_angles ;
 err = err + 360 * (err <= -180) ;
 err = err - 360 * (err > 180) ;
@@ -152,13 +165,17 @@ err_means   =   meanrow(err,ids) ;
 err_var     =    varrow(err,ids) ;
 err_std     =    stdrow(err,ids) ;
 
+disp( sprintf('err_medians [ N=%d th=%d ] = %.1f', N(n), th(m), err_medians(m)) )
+disp( sprintf('err_std     [ N=%d th=%d ] = %.1f', N(n), th(m), err_std(m)) )
+disp( sprintf('detected    [ N=%d th=%d ] = %.1f %%', N(n), th(m), counts(m)/total_counts(m)*100) )
+
 
  
 subplot(4,2,5)
 %plot(x,angles,'o')
 plot(x,err,'o')
 %plot2( ang_cell(:,n), 'o' )
-%plot3( true_ang_cell(:,n), th, 'o' )
+%plot4( true_ang_cell(:,n), th, 'o' )
 grid on
 xlabel('threshold [dBFS]')
 ylabel('angle error (estimated-true) [degrees]')
@@ -169,14 +186,14 @@ subplot(4,2,6)
 %boxplot( err, x, 'notch', 'on' )
 boxplot( err, x, 'notch', 'on', 'Widths', .8*counts/max(counts)+.0001 )
 %boxplot2( ang_cell(:,n), 'notch', 'on' )
-%boxplot3( true_ang_cell(:,n), th, 'notch', 'on' )
+%boxplot4( true_ang_cell(:,n), th, 'notch', 'on' )
 grid on
 xlabel('threshold [dBFS]')
 ylabel('angle error (estimated-true) [degrees]')
 title(sprintf('window size = %d samples \n Box widths are proportional to sample size.',N(n)))
 set(gca,'ytick',[-180 -90 0 90 180])
 
-subplot(4,2,7)
+subplot(4,2,[5 7])
 plot(th,err_medians,'*-')
 plot( th', [err_medians' err_means' err_var'/1000 err_std'], '*-' )
 grid on
